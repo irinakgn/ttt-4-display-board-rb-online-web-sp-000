0cfa32dd34d42c14c5e148682909c7f75994490f
mirthful-flag-5763 <irinagamb@gmail.com>
2019-03-10 23:55:07 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,6 +1,6 @@
-board = [" ", " ", " ", " ", " X ", " ", " ", " ", " "]
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 def display_board(board)
-  puts " X #{board[0]}"
+  puts " X #{board[4]}"
 end
 
 # out the current state.
